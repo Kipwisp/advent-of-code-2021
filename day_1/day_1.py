b21cc95257940b05b3038e2a42111f6be0f31366
Kipwisp <61100140+Kipwisp@users.noreply.github.com>
2021-12-03 13:51:59 -0600

    fix formatting

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -1,49 +1,53 @@
 import time
 
+
 def read_input(path):
     result = []
 
-    with open(path, 'r') as file:
+    with open(path, "r") as file:
         result = file.readlines()
 
         for i, line in enumerate(result):
             result[i] = int(line.strip())
-            
-    
+
     return result
+
 
 def solve_silver(puzzle):
     count = 0
 
     for i in range(1, len(puzzle)):
-        if puzzle[i] > puzzle[i-1]:
+        if puzzle[i] > puzzle[i - 1]:
             count += 1
-    
+
     return count
+
 
 def solve_gold(puzzle):
     count = 0
     window_size = 3
 
     for i in range(window_size, len(puzzle)):
-        if sum(puzzle[i-window_size:i]) > sum(puzzle[i-window_size-1:i-1]):
+        if sum(puzzle[i - window_size : i]) > sum(puzzle[i - window_size - 1 : i - 1]):
             count += 1
-    
+
     return count
 
+
 def main():
-    path = 'input.txt'
+    path = "input.txt"
     puzzle = read_input(path)
 
     start = time.time()
     silver = solve_silver(puzzle)
-    print(f'Silver: {silver}')
-    print(f'Delta: {time.time()-start}\n')
+    print(f"Silver: {silver}")
+    print(f"Delta: {time.time()-start}\n")
 
     start = time.time()
     gold = solve_gold(puzzle)
-    print(f'Gold: {gold}')
-    print(f'Delta: {time.time()-start}\n')
+    print(f"Gold: {gold}")
+    print(f"Delta: {time.time()-start}\n")
+
 
 if __name__ == "__main__":
     main()
